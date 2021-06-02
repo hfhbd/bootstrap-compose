16ba4e9790a326132aaa963ccecad314f1f44a48
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-06-02 21:04:50 +0200

    Add fluid to container

--- src/jsMain/kotlin/app/softwork/bootstrapcompose/Container.kt
+++ src/jsMain/kotlin/app/softwork/bootstrapcompose/Container.kt
@@ -5,12 +5,16 @@ import org.jetbrains.compose.web.dom.*
 
 @Composable
 public fun Container(
+    fluid: Boolean = false,
     type: Breakpoint? = null,
     content: @Composable () -> Unit
 ): Unit = Div(attrs = {
     classes("container")
     if (type != null) {
         classes("container-$type")
+    }
+    if(fluid) {
+        classes("container-fluid")
     }
 }) {
     content()
