f7ec46deb95d28ca6f06076e325d18c5babad8b7
hfhbd <hfhbd@users.noreply.github.com>
2021-06-02 22:52:40 +0200

    Add compose variable tests

--- src/jsTest/kotlin/app/softwork/bootstrapcompose/TableTest.kt
+++ src/jsTest/kotlin/app/softwork/bootstrapcompose/TableTest.kt
@@ -1,9 +1,58 @@
 package app.softwork.bootstrapcompose
 
+import androidx.compose.runtime.*
 import org.jetbrains.compose.web.dom.*
 import kotlin.test.*
 
 class TableTest {
+
+    @Test
+    fun variable() = runTest {
+        val s: @Composable () -> Unit = @Composable { Text("a") }
+        composition {
+            s()
+        }
+        assertEquals("a", root.innerHTML)
+    }
+
+    class Holder {
+        var s: @Composable () -> Unit = { }
+    }
+
+    @Test
+    fun classTest() = runTest {
+        val holder = Holder()
+        holder.s = @Composable { Text("a") }
+        composition {
+            holder.s()
+        }
+        assertEquals("a", root.innerHTML)
+    }
+
+    class HolderConstructor(val s: @Composable () -> Unit)
+
+
+    @Test
+    fun classConstructorTest() = runTest {
+        val holder = HolderConstructor(@Composable { Text("a") })
+        composition {
+            holder.s()
+        }
+        assertEquals("a", root.innerHTML)
+    }
+
+    data class DataHolder(val s: @Composable () -> Unit)
+
+    @Test
+    fun dataClassTest() = runTest {
+        val holder = DataHolder @Composable { Text("a") }
+        composition {
+            holder.s()
+        }
+        assertEquals("a", root.innerHTML)
+    }
+
+
     @Test
     fun table() = runTest {
         composition {
