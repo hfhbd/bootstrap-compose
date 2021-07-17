bd7984e4202bac2c93b1dfcb7c45822121910e0a
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-07-17 02:58:53 +0200

    Add table customization (#64)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- src/jsTest/kotlin/app/softwork/bootstrapcompose/TableTest.kt
+++ src/jsTest/kotlin/app/softwork/bootstrapcompose/TableTest.kt
@@ -102,7 +102,7 @@ class TableTest {
     fun table() = runTest {
         composition {
             Table(listOf("Foo", "Bar")) { _, it ->
-                column("Title", action = {
+                column("Title", header = Header {
                     Button(title = "Text") {
 
                     }
@@ -116,7 +116,7 @@ class TableTest {
         }
 
         assertEquals(
-            expected = """<table class="table"><thead><tr><th scope="col"><div class="row"><div class="col">Title</div><div class="col col-auto"><button type="submit" class="btn btn-primary">Text</button></div></div></th><th scope="col"><div class="row"><div class="col">empty</div><div class="col col-auto"></div></div></th></tr></thead><tbody><tr><td>Foo</td><td></td></tr><tr><td>Bar</td><td></td></tr></tbody></table>""",
+            expected = """<table class="table"><thead><tr><th scope="col"><div class="row"><div class="col">Title</div><div class="col col-auto"><button type="submit" class="btn btn-primary">Text</button></div></div></th><th scope="col">empty</th></tr></thead><tbody><tr><td>Foo</td><td></td></tr><tr><td>Bar</td><td></td></tr></tbody></table>""".trimMargin(),
             actual = root.innerHTML
         )
     }
