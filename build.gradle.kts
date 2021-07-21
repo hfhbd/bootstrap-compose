c5df44b63c71b49d71f9a16d3b89ba3e5c4895a1
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-07-21 10:13:59 +0200

    Remove GitHub Packages repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,6 @@ group = "app.softwork"
 repositories {
     mavenCentral()
     maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven(url = "https://maven.pkg.github.com/hfhbd/*") {
-        credentials {
-            username = System.getProperty("gpr.user") ?: System.getenv("GITHUB_ACTOR")
-            password = System.getProperty("gpr.key") ?: System.getenv("GITHUB_TOKEN")
-        }
-    }
 }
 
 kotlin {
