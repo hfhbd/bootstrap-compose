ef692601f90d898d1542f77f68fc2d51398963a5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-07-21 10:47:30 +0200

    Add mavenCentral publishing (#72)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import java.util.Base64
+
 plugins {
     kotlin("multiplatform") version "1.5.21"
     id("org.jetbrains.compose") version "0.5.0-build262"
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 group = "app.softwork"
@@ -35,15 +39,59 @@ kotlin {
     }
 }
 
+task("emptyJar", Jar::class) {
+}
+
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/hfhbd/bootstrap-compose")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+    publications.all {
+        if (this is MavenPublication) {
+            artifact(tasks.getByName("emptyJar")) {
+                classifier = "javadoc"
             }
+            pom {
+                name.set("app.softwork Bootstrap Library for JetBrains Compose Web")
+                description.set("A wrapper for Bootstrap to use with JetBrains Compose Web")
+                url.set("https://github.com/hfhbd/bootstrap-compose")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("hfhbd")
+                        name.set("Philip Wedemann")
+                        email.set("mybztg+mavencentral@icloud.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git://github.com/hfhbd/bootstrap-compose.git")
+                    developerConnection.set("scm:git://github.com/hfhbd/bootstrap-compose.git")
+                    url.set("https://github.com/hfhbd/bootstrap-compose")
+                }
+            }
+        }
+    }
+}
+(System.getProperty("signing.privateKey") ?: System.getenv("SIGNING_PRIVATE_KEY"))?.let {
+    String(Base64.getDecoder().decode(it)).trim()
+}?.let { key ->
+    println("found key, config signing")
+    signing {
+        val signingPassword = System.getProperty("signing.password") ?: System.getenv("SIGNING_PASSWORD")
+        useInMemoryPgpKeys(key, signingPassword)
+        sign(publishing.publications)
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            username.set(System.getProperty("sonartype.apiKey") ?: System.getenv("SONARTYPE_APIKEY"))
+            password.set(System.getProperty("sonartype.apiToken") ?: System.getenv("SONARTYPE_APITOKEN"))
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
