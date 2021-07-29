b4ee62eb1047501e7da79974aa33a1e50d98555b
Greg Steckman <gsteckman@gmail.com>
2021-07-29 01:54:38 -0700

    Modal dialog updates to behave more like Compose components (#81)
    * Add attrs.
    
    * Add text-truncate class to Text styling options.
    
    * Make Modal behave more Compose-like.

--- src/jsMain/kotlin/app/softwork/bootstrapcompose/Modal.kt
+++ src/jsMain/kotlin/app/softwork/bootstrapcompose/Modal.kt
@@ -6,67 +6,77 @@ import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.*
 
+
 @Composable
 public fun Modal(
     header: String,
-    buttonTitle: String,
     size: Breakpoint? = null,
     scrollable: Boolean = false,
     id: String = UUID().toString(),
-    buttonColor: Color = Color.Primary,
     styling: (Styling.() -> Unit)? = null,
+    onDismissRequest: () -> Unit,
     footer: ContentBuilder<HTMLDivElement>? = null,
     content: ContentBuilder<HTMLDivElement>
 ) {
+    var bsModal: Modal? = null //Bootstrap Modal class
+
     val classes = styling?.let {
         Styling().apply(it).generate()
     } ?: arrayOf()
 
-    Div {
-        Button({
-            classes("btn", "btn-$buttonColor")
-            attr("data-bs-toggle", "modal")
-            attr("data-bs-target", "#_$id")
-        }) {
-            Text(buttonTitle)
+    Div({
+        classes("modal")
+        classes(*classes)
+        id("_$id")
+        tabIndex(-1)
+        attr("aria-labelledby", "label$id")
+        attr("aria-hidden", "true")
+        attr("data-bs-backdrop", "static")
+        attr("data-bs-keyboard", "false")
+    }) {
+        DisposableRefEffect { htmlDivElement: HTMLDivElement ->
+            bsModal = Modal(htmlDivElement)
+            htmlDivElement.addEventListener("hidePrevented.bs.modal", callback = { _ ->
+                onDismissRequest()
+            })
+            bsModal?.show()
+            onDispose {
+                bsModal?.hide()
+                bsModal?.dispose()
+            }
         }
-        Div({
-            classes("modal")
-            id("_$id")
-            tabIndex(-1)
-            attr("aria-labelledby", "label$id")
-            attr("aria-hidden", "true")
-        }) {
 
+        Div({
+            classes("modal-dialog")
+            if (size != null) {
+                classes("modal-$size")
+            }
+            if (scrollable) {
+                classes("modal-dialog-scrollable")
+            }
+        }) {
             Div({
-                classes("modal-dialog")
-                if (size != null) {
-                    classes("modal-$size")
-                }
-                if (scrollable) {
-                    classes("modal-dialog-scrollable")
-                }
+                classes("modal-content")
+                classes(*classes)
             }) {
-                Div({
-                    classes("modal-content")
-                    classes(*classes)
-                }) {
-                    Div({ classes("modal-header") }) {
-                        H5({
-                            classes("modal-title")
-                            id("label$id")
-                        }) {
-                            Text(header)
-                        }
-                        Button({
-                            classes("btn-close")
-                            attr("data-bs-dismiss", "modal")
-                            attr("aria-label", "Close")
-                        })
+                Div({ classes("modal-header") }) {
+                    H5({
+                        classes("modal-title")
+                        id("label$id")
+                    }) {
+                        Text(header)
                     }
-                    Div({ classes("modal-body") }, content)
-                    Div({ classes("modal-footer") }, footer)
+                    Button({
+                        classes("btn-close")
+                        //attr("data-bs-dismiss", "modal")
+                        attr("aria-label", "Close")
+                        onClick {
+                            onDismissRequest()
+                        }
+                    })
                 }
+                Div({ classes("modal-body") }, content)
+                Div({ classes("modal-footer") }, footer)
             }
         }
     }
