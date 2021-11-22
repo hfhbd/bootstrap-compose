be2f50bbf129ebad486298b4462f0076688cbfb0
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-11-22 21:05:45 +0100

    Refactor ZIndex using external JS function (#151)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- src/jsMain/kotlin/app/softwork/bootstrapcompose/ZIndex.kt
+++ src/jsMain/kotlin/app/softwork/bootstrapcompose/ZIndex.kt
@@ -1,10 +1,18 @@
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
+)
+
 package app.softwork.bootstrapcompose
 
-public interface ZIndex {
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto'}/*union*/)""")
+public sealed external interface ZIndex {
     public companion object {
-        public inline val auto: ZIndex get() = ZIndex("auto")
+        public val auto: ZIndex
     }
 }
 
-public inline fun ZIndex(value: String): ZIndex = value.unsafeCast<ZIndex>()
-public inline fun ZIndex(value: Int): ZIndex = ZIndex(value = value.toString())
+public inline fun ZIndex(value: Int): ZIndex =
+    value.unsafeCast<ZIndex>()
