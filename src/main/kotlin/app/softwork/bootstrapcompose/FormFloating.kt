1e17f95e3a09cd043c218a68d67e428af959702f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-06-05 17:49:20 +0200

    Add floating forms (#217)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

---
+++ src/main/kotlin/app/softwork/bootstrapcompose/FormFloating.kt
@@ -0,0 +1,25 @@
+package app.softwork.bootstrapcompose
+
+import androidx.compose.runtime.*
+import org.jetbrains.compose.web.dom.*
+import org.w3c.dom.*
+
+@Composable
+@NonRestartableComposable
+public fun FormFloating(
+    styling: (Styling.() -> Unit)? = null,
+    attrs: AttrBuilderContext<HTMLDivElement>? = null,
+    content: ContentBuilder<HTMLDivElement>? = null
+) {
+    Style
+    val classes = styling?.let {
+        Styling().apply(it).generate()
+    } ?: arrayOf()
+    Div(attrs = {
+        classes("form-floating")
+        classes(classes = classes)
+        if (attrs != null) {
+            attrs()
+        }
+    }, content)
+}
