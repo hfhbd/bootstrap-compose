95479b430a2669c1314c4686de1eed6fe3aa7f78
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-08-30 09:46:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ import org.jetbrains.compose.*
 import java.util.*
 import io.gitlab.arturbosch.detekt.*
 
+val composeVersion = "1.2.0-alpha01-dev774"
+
 plugins {
     kotlin("js") version "1.7.10"
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev774"
+    id("org.jetbrains.compose") version "$composeVersion"
     `maven-publish`
     signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
@@ -47,10 +49,10 @@ dependencies {
 
 licensee {
     allow("Apache-2.0")
-    allowDependency("org.jetbrains.compose.web", "web-core-js", "1.2.0-alpha01-dev770")
-    allowDependency("org.jetbrains.compose.web", "web-core", "1.2.0-alpha01-dev770")
-    allowDependency("org.jetbrains.compose.web", "internal-web-core-runtime-js", "1.2.0-alpha01-dev770")
-    allowDependency("org.jetbrains.compose.web", "internal-web-core-runtime", "1.2.0-alpha01-dev770")
+    allowDependency("org.jetbrains.compose.web", "web-core-js", "$composeVersion")
+    allowDependency("org.jetbrains.compose.web", "web-core", "$composeVersion")
+    allowDependency("org.jetbrains.compose.web", "internal-web-core-runtime-js", "$composeVersion")
+    allowDependency("org.jetbrains.compose.web", "internal-web-core-runtime", "$composeVersion")
 }
 
 val emptyJar by tasks.registering(Jar::class) {
