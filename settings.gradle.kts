5b289e0a24b26991c71a3949040a5c75f1f156ce
hfhbd <hfhbd@users.noreply.github.com>
2023-09-23 23:54:35 +0200

    Add Gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
 
 plugins {
     id("MyRepos")
+    id("com.gradle.enterprise") version "3.15"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
