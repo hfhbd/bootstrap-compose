3dd6c7b5b07107a8239f4374df291f29baabcfa5
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-05-20 11:40:20 +0200

    Bump Kotlin to 2.0.0

--- showcase/build.gradle.kts
+++ showcase/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("setup")
-    id("org.jetbrains.compose")
+    kotlin("plugin.compose")
 }
 
 kotlin {
@@ -12,6 +12,7 @@ kotlin {
 }
 
 dependencies {
+    implementation(libs.coroutines.core)
     implementation(projects.bootstrapCompose)
     implementation(projects.bootstrapCompose.bootstrapComposeIcons)
 }
