7be39cf72c579bf0abb0ad8560a10fcc8a1db7bd
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-05-20 11:18:27 +0200

    Refactor Gradle setup (#383)
    * Refactor Gradle setup
    
    * Return yarn lock until Kotlin 2.0

--- bootstrap-compose-icons/build.gradle.kts
+++ bootstrap-compose-icons/build.gradle.kts
@@ -3,13 +3,15 @@ plugins {
     id("sign")
 }
 
-
 val generateSVG by tasks.registering(app.softwork.bootstrapcompose.icons.ConvertSvg::class) {
     dependsOn(rootProject.tasks.named("kotlinNpmInstall"))
     icons.set(rootProject.file("build/js/node_modules/bootstrap-icons/icons"))
     outputDir.set(project.layout.buildDirectory.dir("generated/icons"))
 }
-kotlin.sourceSets["main"].kotlin.srcDir(generateSVG)
+
+kotlin.sourceSets.main {
+    kotlin.srcDir(generateSVG)
+}
 
 dependencies {
     api(compose.html.core)
