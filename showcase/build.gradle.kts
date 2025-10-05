fc0e1a523a1fca8ae03d944a6ac1f6cb58e98c76
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-10-05 17:06:26 +0200

    Use ES modules in showcase (#472)

--- showcase/build.gradle.kts
+++ showcase/build.gradle.kts
@@ -6,6 +6,10 @@ kotlin {
     js {
         browser {
             binaries.executable()
+            useEsModules()
+            compilerOptions {
+                target.set("es2015")
+            }
         }
     }
 
