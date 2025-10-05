52a80ca6e603efaaa39707db17711521ed682539
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2025-10-05 17:41:23 +0200

    Update boostrap to 5.3.8 and bootstrap icons to 1.13.1 (#473)

--- bootstrap-compose/build.gradle.kts
+++ bootstrap-compose/build.gradle.kts
@@ -8,8 +8,8 @@ kotlin {
             dependencies {
                 api(libs.compose.runtime)
                 api(libs.compose.html.core)
-                api(npm("bootstrap", "5.2.3"))
-                api(npm("@popperjs/core", "2.11.6"))
+                api(npm("bootstrap", "5.3.8"))
+                api(npm("@popperjs/core", "2.11.8"))
             }
         }
         jsTest {
